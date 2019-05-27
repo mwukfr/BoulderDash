3abb8b4d6769646d8f7eb5e6f17955e99a07182a
Maxim <maximw85@gmail.com>
2019-05-27 14:51:17 +0200

    Code bouchonné diagramme {entity}

---
+++ entity/src/main/java/entity/Player.java
@@ -0,0 +1,37 @@
+package entity;
+
+/**
+ * Player class inherits of Mobile.
+ * @author Sacha
+ *
+ */
+public class Player extends Mobile {
+
+	/**
+	 * Variables.
+	 */
+	public Player player;
+	
+	/**
+	 * Getter of player.
+	 * @return
+	 */
+	public Player getPlayer() {
+		return player;
+	}
+	
+	/**
+	 * Setter of player.
+	 * @param player
+	 */
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+	
+	/**
+	 * Constructor of the Player class.
+	 */
+	public Player() {
+		
+	}
+}
