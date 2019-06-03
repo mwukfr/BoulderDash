61d3feec6ef850c9d576d4a71cac080c52ffd05d
Orme21 <44492129+Orme21@users.noreply.github.com>
2019-06-03 09:11:52 +0200

    Update Map.java

--- model/src/main/java/model/Map.java
+++ model/src/main/java/model/Map.java
@@ -1,7 +1,9 @@
 package model;
 import java.util.*;
+import entity.*;
+import java.sql.*;
 
-public class Map {
+public class DAOMap {
 	
 	//private int level = ;
 	
@@ -44,43 +46,66 @@ public class Map {
 	}
 	
 	/**
-     * Creates the maps.
+     * Creates the map.
      */
-	/*
-	 * public static ArrayList<Objet> mapImage(){
-	 * 
-	 * ArrayList<Objet> tabObjet=new ArrayList<Objet>();
-	 * 	for(int X=0;X<24;X++) {
-	 * 	System.out.println(level.getMap()[X][Y]);
-	 *  switch(level.getMap()[X][Y]){
-	 *  
-	 *  case("-") (add new "dirt")
-		break
+
+	public void getLevel() {
+	try {
+		final String sql = "{call LevelOne}";
+		final CallableStatement call = this.getConnection().prepareCall(sql);
+		call.execute();
+		System.out.println("Maxim si tu lis ça c'est que tout marche à peu près");
+		final ResultSet resultSet = call.getResultSet();
+		if (resultSet.first()) {
+			helloWorld = new HelloWorld(resultSet.getInt("id"), code, resultSet.getString("message"));
+		}
+		return helloWorld;
+	} catch (final SQLException e) {
+		e.printStackTrace();
+	}
+	}
+	
+	  public static ArrayList<Objects> mapImage(){
+	  
+	  ArrayList<Objet> tabObjet=new ArrayList<Objet>();
+	  	for(int X=0;X<24;X++) {
+	  	System.out.println(level.getMap()[X][Y]);
+	 	switch(level.getMap()[X][Y]){
+	 
+	 	case("t"):
+	 		 Dirt dirt = new Dirt();
+		break;
 		
-		case("_") (add new "Empty_dirt")
-		break
+		case("e"):
+			EmptyDirt emptyDirt = new EmptyDirt();
+		break;
 		
-		case("µ")(add new "wall")
-		break
+		case("X"):
+			Wall wall = new Wall();
+		break;
+	
+		case("b"):
+			 Boulder boulder = new Boulder();
+		break;
 		
-		case("X") (add new "boulder")
-		break
+		case("d"):
+			Diamond diamond = new Diamond();
+		break;
 		
-		case("D") (add new "diamond")
-		break
+		case("p"):
+			 Player player = new Player();
+		break;
 		
-		case("0") (add new "Player")
-		break
-		
-		case("§")(add new "Exit")
-		break
+		case("s"):
+			Exit exit = new Exit();
+		break;
+			
 		default:
-		System.out.println("OOF")
+			System.out.println("OOF");
 		break;
 		}
 		}
 		System.out.println("\n");
 		}
 		
-	 	*/
 }
